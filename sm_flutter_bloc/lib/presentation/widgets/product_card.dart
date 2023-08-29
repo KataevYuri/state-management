e9d3db84984f07c0cac75d506e294603463a1adc
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-08-29 11:14:34 +0800

    1

--- sm_flutter_bloc/lib/presentation/widgets/product_card.dart
+++ sm_flutter_bloc/lib/presentation/widgets/product_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:simpe_state_management/domain/providers/sm_provider.dart';
+import 'package:simpe_state_management/domain/providers/counter_bloc.dart';
 import 'package:simpe_state_management/presentation/pages/home_page.dart';
 
 import '../../domain/models/product.dart';
